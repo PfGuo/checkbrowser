afe837a66bef587cbb3a56cb34212a5d5b9ad0e2
guopengfei <guopengfei@kedacom.com>
2018-01-29 15:04:49 +0800

    添加获取浏览器版本

---
+++ checkbrowser.js
@@ -0,0 +1,8 @@
+(function () {
+    var browser = window.navigator;
+    var _platform = browser.platform;
+    var _userAgent = browser.userAgent;
+    var _appCodeName = browser.appCodeName;
+
+    console.log(browser);
+})()
